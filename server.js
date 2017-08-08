13c6aa8242607602756e131910cd4acd596916c9
jasmine-wadhwania <jasmine.wadhwania@gmail.com>
2017-08-08 19:30:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -50,7 +50,7 @@ var htmlTemplate=`<html>
 return htmlTemplate;
 }
 app.get('/', function (req, res) {
-  res.sendFile(creaTemplate(article-one));
+  res.sendFile(createTemplate(article-one));
 });
 app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
