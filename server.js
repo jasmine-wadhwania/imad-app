55ac05b0b7a3f91cf05446574606006df08aac67
jasmine-wadhwania <jasmine.wadhwania@gmail.com>
2017-08-08 18:49:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res) {
   res.send('Article one requested and will be served');
@@ -18,7 +18,7 @@ app.get('/article-three', function (req, res) {
   res.send('Article one requested and will be served');
 });
 app.get('/ui/style.css', function (req, res) {
-  res.send('Article one requested and will be served');
+  res.sendFile(path.join(__dirname,'ui','style.css'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
