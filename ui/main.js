5fb88566f9622608b97c363ae8b60fa761eccf05
jasmine-wadhwania <jasmine.wadhwania@gmail.com>
2017-08-08 19:57:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,8 @@
 console.log('Loaded!');
 var img=document.getElementById('madi');
+var marginLeft=0;
 function moveRight(){
-    marginLeft=0;
+   
     marginLeft=marginLeft+10;
     img.style.marginLeft=marginLeft+'px';
 }
